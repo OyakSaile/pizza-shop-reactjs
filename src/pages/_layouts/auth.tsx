cf07dab9ba6bb70e0e8c7576dc051043346a08e8
kayo elias <contatodevkayo@gmail.com>
2024-03-06 00:47:46 -0300

    feat: initial dashboard layout

--- src/pages/_layouts/auth.tsx
+++ src/pages/_layouts/auth.tsx
@@ -2,7 +2,7 @@ import { Outlet } from 'react-router-dom'
 import { Pizza } from 'lucide-react'
 export function AuthLayout() {
   return (
-    <div className="grid min-h-screen lg:grid-cols-2">
+    <div className="grid min-h-screen antialiased lg:grid-cols-2">
       <div className="flex h-full flex-col justify-between border-r border-foreground/5 bg-muted p-10 text-muted-foreground">
         <div className="flex items-center gap-3 text-lg text-foreground">
           <Pizza className="h-5 w-5" />
