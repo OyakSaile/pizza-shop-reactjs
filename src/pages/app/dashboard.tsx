a968e87e8dbddc42d8f02d387ae0af712039c8fc
kayo elias <contatodevkayo@gmail.com>
2024-03-05 23:39:08 -0300

    feat: sign page

--- src/pages/app/dashboard.tsx
+++ src/pages/app/dashboard.tsx
@@ -4,7 +4,17 @@ export function Dashboard() {
   return (
     <>
       <Helmet title="Dashboard" />
-      <h1>SignIn</h1>
+      <div className="p-8">
+        <div className="flex w-[350px] flex-col justify-center gap-6">
+          <div className="flex flex-col gap-2 text-center">
+            <h1 className="text-2xl font-semibold tracking-tight">
+              Acessar painel
+            </h1>
+            <p>Acompanhe suas vendas pelo painel do parceiro!</p>
+          </div>
+          <div></div>
+        </div>
+      </div>
     </>
   )
 }
