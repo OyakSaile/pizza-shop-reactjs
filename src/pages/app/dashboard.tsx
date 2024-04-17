672ece6075db5a468b00489e63ad31c32206f590
kayo elias <contatodevkayo@gmail.com>
2024-04-17 12:53:55 -0300

    feat : menu

--- src/pages/app/dashboard.tsx
+++ src/pages/app/dashboard.tsx
@@ -5,15 +5,7 @@ export function Dashboard() {
     <>
       <Helmet title="Dashboard" />
       <div className="p-8">
-        <div className="flex w-[350px] flex-col justify-center gap-6">
-          <div className="flex flex-col gap-2 text-center">
-            <h1 className="text-2xl font-semibold tracking-tight">
-              Acessar painel
-            </h1>
-            <p>Acompanhe suas vendas pelo painel do parceiro!</p>
-          </div>
-          <div></div>
-        </div>
+        <h1>Dashboard</h1>
       </div>
     </>
   )
